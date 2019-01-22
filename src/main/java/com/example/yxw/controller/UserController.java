ea4ff553a526f3772d6d0aafb9ec9241b5f63544
caoli <Cl@1234>
2019-01-22 16:42:10 +0800

    静态资源以及怪兽实体提价

--- src/main/java/com/example/yxw/controller/UserController.java
+++ src/main/java/com/example/yxw/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController("/")
 public class UserController {
@@ -17,5 +18,12 @@ public class UserController {
     @RequestMapping("show")
     public User getIndex(String username) {
         return userService.selectUserByName(username);
+    }
+
+    @RequestMapping("index")
+    public ModelAndView getIndex() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("layout1.html");
+        return modelAndView;
     }
 }
