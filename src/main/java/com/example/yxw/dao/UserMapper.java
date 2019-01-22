c7b45285bca8248d152a277856d8a8002c9ad1e7
caoli <Cl@1234>
2019-01-22 08:57:58 +0800

    修改sql

--- src/main/java/com/example/yxw/dao/UserMapper.java
+++ src/main/java/com/example/yxw/dao/UserMapper.java
@@ -1,9 +1,10 @@
 package com.example.yxw.dao;
 
 import com.example.yxw.domain.User;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserMapper {
-    User selectUserByName(String username);
+    User selectUserByName(@Param(value = "username") String username);
 }
